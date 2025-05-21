f2fb514e1c59026eda07918b61d810e4d2e40bee
Luong Nguyen Khoi <khoinguyenluong012@gmail.com>
2025-05-21 18:25:45 +0700

    Add functionality to retrieve appointments by user
    Introduced a new endpoint to fetch appointments based on user role (doctor or patient) in `ApiAppointmentController`. Implemented corresponding methods in `AppointmentService` and `AppointmentRepository` to fetch appointment data. This adds role-specific appointment management capabilities to the API.

--- src/main/java/com/kh/repositories/AppointmentRepository.java
+++ src/main/java/com/kh/repositories/AppointmentRepository.java
@@ -5,9 +5,13 @@ import com.kh.pojo.Appointment;
 import com.kh.pojo.User;
 
 import java.util.Date;
+import java.util.List;
 
 public interface AppointmentRepository {
     Appointment add(Appointment appointment);
 
     boolean isDoctorTimeSlotTaken(User doctor, Date date, AppointmentSlot slot);
+    
+    List<Appointment> findByPatientId(Long patientId);
+    List<Appointment> findByDoctorId(Long doctorId);
 }
